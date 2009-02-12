ff067c1ac5232478482b6d6bf63ed44bb971e5d7
Csaba Hoch <csaba.hoch@gmail.com>
2009-02-12 10:53:05 +0100

    test.py can run a specific test.

--- test.py
+++ test.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python
+"""
+Tests the Heapmanipulator.
 
+If executed without arguments, it runs all tests:
+
+    python test.py
+
+It can be used to run a specific test:
+
+    python test.py test_heapmanip TestMailDB2 testThreadstructCycle1
+"""
+
+import sys
 import unittest
 import heapmanip
 import test_heapmanip
 import test_heapia
 
 if __name__ == '__main__':
+    args = sys.argv[1:]
     heapmanip.set_log(False)
-    suite1 = unittest.TestLoader().loadTestsFromModule(test_heapmanip)
-    suite2 = unittest.TestLoader().loadTestsFromModule(test_heapia)
-    suite = unittest.TestSuite([suite1, suite2])
+    if args == []:
+        suite1 = unittest.TestLoader().loadTestsFromModule(test_heapmanip)
+        suite2 = unittest.TestLoader().loadTestsFromModule(test_heapia)
+        suite = unittest.TestSuite([suite1, suite2])
+    elif len(args) == 3:
+        modname, testcasename, funname = args
+        suite = unittest.TestSuite()
+        testcase = getattr(__import__(modname), testcasename)
+        suite.addTest(testcase(funname))
     unittest.TextTestRunner(verbosity=0).run(suite)
 
