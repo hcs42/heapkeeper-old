7f867a3bd53d8fc93223390f916d274228dcc19e
Csaba Hoch <csaba.hoch@gmail.com>
2009-05-01 07:14:38 +0200

    Doc/conf.py: Heapmanipulator -> Heapkeeper
    The configuration file has been updated to contain "Heapkeeper" as the project
    name instead of "Heapmanipulator".

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Heapmanipulator documentation build configuration file, created by
+# Heapkeeper documentation build configuration file, created by
 # sphinx-quickstart on Sat Mar 28 19:42:44 2009.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -37,7 +37,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Heapmanipulator'
+project = u'Heapkeeper'
 copyright = u'2009, Csaba Hoch, Attila Nagy'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -158,7 +158,7 @@ html_static_path = ['_static']
 #html_file_suffix = ''
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Heapmanipulatordoc'
+htmlhelp_basename = 'Heapkeeperdoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -172,7 +172,7 @@ htmlhelp_basename = 'Heapmanipulatordoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Heapmanipulator.tex', u'Heapmanipulator Documentation',
+  ('index', 'Heapkeeper.tex', u'Heapkeeper Documentation',
    u'Csaba Hoch, Attila Nagy', 'manual'),
 ]
 
